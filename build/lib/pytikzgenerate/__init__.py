42e32ca7c3404c27b1f60299772cf3e4fb0d02f7
Juan Serrano <Juanfater2013@hotmail.com>
2021-09-20 20:04:00 -0500

    Correccion .gitignore

--- build/lib/pytikzgenerate/__init__.py
+++
@@ -1,2 +0,0 @@
-"""Supermodulo encargado del procesamiento logico en la indentacion y depuracion del codigo TikZ, transpilacion a codigo PyTikZ y la ejecucion de los comandos TikZ ya transpilados y validados para ser graficados en un Widget Kivy.
-"""
